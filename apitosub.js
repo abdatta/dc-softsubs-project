627c79c83f9bc21159ce168ac106d0f402f0699f
Abhishek Datta <abdatta@iitk.ac.in>
2020-02-09 14:07:57 +0530

    Replace with a better text blocks grouping algo

--- apitosub.js
+++ apitosub.js
@@ -1,58 +1,35 @@
 const fs = require('fs');
 const moment = require('moment');
 
-const getTextBlocks = (visionResults) => {
-    let textBlocks = [];
-    // visionResults.forEach(result => {
-    //     result.fullTextAnnotation.pages.forEach(page => {
-    //         textBlocks = textBlocks.concat(page.blocks.map(block => ({
-    //           y: parseInt(block.boundingBox.vertices[0].y / 120) + 1,
-    //           text: getBlockText(block).trim()
-    //         })))
-    //     });
-    // });
-    // console.log(textBlocks);
-    // process.exit(1);
-    visionResults.forEach(result => {
-      result.fullTextAnnotation.pages.forEach(page => {
-          page.blocks.forEach(block => {
-            const i = parseInt(block.boundingBox.vertices[0].y / 120);
-            const text = getBlockText(block).trim();
-            if (i < textBlocks.length) {
-              textBlocks[i] += '\n' + text;
-            } else if (i === textBlocks.length) {
-              textBlocks.push(text);
-            } else {
-              console.log('Error - block skipped! Should be: ' + textBlocks.length + '  Found: ' + i);
-              console.log(textBlocks);
-              process.exit(1);
-            }
-          })
-      });
-    });
-    return textBlocks;
-}
-
-const getBlockText = (block) => {
-    let result = '';
-    block.paragraphs.forEach(paragraph => {
-        paragraph.words.forEach(word => {
+const groupIntoBlocks = (apiResults) => {
+  let texts = [];
+  apiResults.forEach(result => {
+    result.fullTextAnnotation.pages.forEach(page => {
+      page.blocks.forEach(block => {
+        block.paragraphs.forEach(paragraph => {
+          paragraph.words.forEach(word => {
             word.symbols.forEach(symbol => {
-                result += symbol.text;
+                const i = parseInt((symbol.boundingBox.vertices[0].y + 10) / 120); // divisions at 110, 230, 350,...
+                let len = texts.length;
+                if (i >= len) for(; len<=i; len++) texts.push('');
+                texts[i] += symbol.text;
                 if (symbol.property && symbol.property.detectedBreak) {
                     const breakType = symbol.property.detectedBreak.type;
                     if (['EOL_SURE_SPACE' ,'SPACE'].includes(breakType)) {
-                        result += " ";
+                      texts[i] += ' ';
                     }
                     if (['EOL_SURE_SPACE' ,'LINE_BREAK'].includes(breakType)) {
-                        result += "\n"; // Perhaps use os.EOL for correctness.
+                      texts[i] += '\n'; // Perhaps use os.EOL for correctness.
                     }
                 }
-            })
-        })
-    })
+            });
+          });
+        });
+      });
+    });
+  });
 
-    return result;
+  return texts;
 }
 
 const epNo = process.argv[2];
@@ -70,7 +47,9 @@ for (let p = parseInt(startPartNo); p <= parseInt(endPartNo); p++) {
   const curr_dir = 'public/episode' + epNo + '/part_' + String(p).padStart(2, '0') + '/';
   const results = JSON.parse(fs.readFileSync(curr_dir+'ocr_results.json'));
   const files = fs.readdirSync(curr_dir+'paired').filter(s => s.endsWith('.jpg'));
-  const blocks = getTextBlocks(results.responses);
+
+  const blocks = groupIntoBlocks(results.responses).map(text => text.trim());
+
   if (files.length !== blocks.length) {
       console.log('blocks and files not of same sizes!', 'block size: ', blocks.length, 'files:', files.length);
       process.exit(1);
@@ -82,17 +61,19 @@ for (let p = parseInt(startPartNo); p <= parseInt(endPartNo); p++) {
       filename: file,
       start: match && parseInt(match[1]),
       end: match && parseInt(match[2]),
-      startTS: match && moment.utc(parseInt(match[1]) * 1000 / fps).format('HH:mm:ss.SS'),
-      endTS: match && moment.utc(parseInt(match[2]) * 1000 / fps).format('HH:mm:ss.SS'),
-      text: blocks[i]
+      startTS: match && moment.utc(parseInt(match[1]) * 1000 / fps - 10).format('H:mm:ss.SS'),
+      endTS: match && moment.utc(parseInt(match[2]) * 1000 / fps + 10).format('H:mm:ss.SS'),
+      text: blocks[i].replace(/\n/g, ' ').replace(/\s+/g, ' ')
     }
   }));
 }
 
 const subtemp = fs.readFileSync('subtemp.ass', 'utf8');
 const finalsubs = subtemp + submap.map(
-      sub => `Dialogue: 0,${sub.startTS},${sub.endTS},DCMain,,0000,0000,0000,,${sub.text.replace(/\n/g, ' ').replace(/\s+/g, ' ')}`
+      sub => `Dialogue: 0,${sub.startTS},${sub.endTS},DCMain,,0,0,0,,${sub.text}`
   ).join('\n') + '\n';
 const finalsubfile = `public/episode${epNo}/ocr_subs_${epNo}_${startPartNo}${endPartNo === startPartNo ? '' : '_' + endPartNo}.ass`;
 fs.writeFileSync(finalsubfile, finalsubs);
 console.log('Subs written to file:', finalsubfile);
+
+submap.forEach(sub => console.log(sub.text));
